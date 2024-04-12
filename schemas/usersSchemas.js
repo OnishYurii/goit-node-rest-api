1fe861be677a6e0c79abed170a018cd9519180bf
OnishYurii <onishchenkoyurii@gmail.com>
2024-04-12 20:55:31 +0300

    registration in progress

---
+++ schemas/usersSchemas.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+
+export const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegex).required(),
+  password: Joi.string().min(8).required(),
+  subscription: Joi.string().optional(),
+});

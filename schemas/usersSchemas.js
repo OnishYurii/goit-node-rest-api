ea85e4b65ccb9e8f019ecd80b1e99ce3b1bc523d
OnishYurii <onishchenkoyurii@gmail.com>
2024-04-18 14:25:17 +0300

    usersSchemas update

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -3,12 +3,24 @@ import Joi from "joi";
 const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
 
 export const registerSchema = Joi.object({
-  email: Joi.string().pattern(emailRegex).required(),
-  password: Joi.string().min(8).required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.pattern.base": "Email is invalid",
+    "any.required": "Email is required",
+  }),
+  password: Joi.string().min(8).required().messages({
+    "string.min": "Password should contain min 8 symbols",
+    "any.required": "Password is required",
+  }),
   subscription: Joi.string().optional(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().pattern(emailRegex).required(),
-  password: Joi.string().min(8).required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.pattern.base": "Email is invalid",
+    "any.required": "Email is required",
+  }),
+  password: Joi.string().min(8).required().messages({
+    "string.min": "Password should contain min 8 symbols",
+    "any.required": "Password is required",
+  }),
 });
